2dd92ad618818845525654400a68fa3c8f867cbf
Kushagra Jaiswal <kjjaiswal2003@gmail.com>
2026-03-15 13:31:07 +0530

    A simple Computer Quiz Game built using Python that tests the user's knowledge of computer fundamentals. The game asks multiple-choice questions and provides instant feedback on correct or incorrect answers while keeping track of the user's score.

---
+++ project1.py
@@ -0,0 +1,38 @@
+# Quiz Game
+print("Welcome to my computer quiz!")
+
+playing = input("do you want to play? ")
+if playing != 'yes':
+    quit()
+print("Okay! Let's play ")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower().strip() == "central processing unit":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does GPU stand for? ")
+if answer.lower().strip() == "graphic processing unit":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does RAM stand for? ")
+if answer.lower().strip() == "random access memory":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+answer = input("What does PSU stand for? ")
+if answer.lower().strip() == "power supply unit":
+    print("Correct")
+    score += 1
+else:
+    print("Incorrect")
+
+print(f"You got {score} questions correct")
